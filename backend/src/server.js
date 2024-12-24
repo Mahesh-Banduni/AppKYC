5a92d757f4fa1bd5aad99a1c06cca0d53631aa3d
Mahesh-Banduni <maheshbanduni9997@gmail.com>
2024-12-24 12:11:32 +0530

    added code

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,13 +27,13 @@ app.use(rendertron.makeMiddleware({
 }));
 
 // Middleware
-//app.use(express.json());
+app.use(express.json());
 // Serve static files from React frontend
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 // Send React app for all other requests (Client-Side Rendering)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
 });
 
 app.use(compression());
